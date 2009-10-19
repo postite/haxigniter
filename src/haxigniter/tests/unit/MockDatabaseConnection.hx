2633e85046f7d096212608a414865673c04641ec
Andreas Soderlund <ciscoheat@gmail.com>
2009-10-19 13:30:41 +0200

    Moved the Mockdatabase class to its own file and renamed to MockDatabaseConnection.

---
+++ src/haxigniter/tests/unit/MockDatabaseConnection.hx
@@ -0,0 +1,78 @@
+package haxigniter.tests.unit;
+
+import haxigniter.libraries.Database;
+
+#if php
+import php.db.Connection;
+import php.db.ResultSet;
+#elseif neko
+import neko.db.Connection;
+import neko.db.ResultSet;
+#end
+
+class MockConnection implements Connection
+{
+	public var queries : Array<String>;
+	
+	public function new()
+	{
+		queries = new Array<String>();
+	}
+	
+	public function close() : Void {}
+	public function commit() : Void {}
+	public function dbName() : String { return 'mockdb'; }
+	public function escape( s : String ) : String { return 'E*' + s + '*E'; }
+	public function lastInsertId() : Int { return 0; }
+	public function quote( s : String ) : String { return 'Q*' + s + '*Q'; }
+	
+	public function request( sql : String ) : ResultSet
+	{ 
+		queries.push(sql);
+		return new MockResultSet();
+	}
+	
+	public function rollback() : Void {}
+	public function startTransaction() : Void {}
+}
+
+class MockDatabaseConnection extends DatabaseConnection
+{
+	private var mockConnection : MockConnection;
+	
+	public var queries(getQueries, null) : Array<String>;
+	public function getQueries() : Array<String>
+	{
+		return this.mockConnection.queries;
+	}
+	
+	public function new()
+	{
+		this.mockConnection = new MockConnection();
+	}
+	
+	public override function open()
+	{
+		if(this.connection != null)
+			throw new DatabaseException('Connection is already open.', this);
+
+		this.connection = this.mockConnection;
+	}
+}
+
+class MockResultSet implements ResultSet
+{
+	public function new() { }
+	
+	private function getLength() : Int { return 0; }
+	private function getNFields() : Int { return 0; }
+	
+	public var length(getLength, null) : Int;
+	public var nfields(getNFields, null) : Int;
+	public function getFloatResult(n : Int) : Float { return 0; }
+	public function getIntResult(n : Int) : Int { return 0; }
+	public function getResult(n : Int) : String { return ''; }
+	public function hasNext() : Bool { return false; }
+	public function next() : Dynamic { return null; }
+	public function results() : List<Dynamic> { return new List(); }
+}
--- src/haxigniter/tests/unit/When_using_library_Database.hx
+++ src/haxigniter/tests/unit/When_using_library_Database.hx
@@ -1,67 +1,14 @@
 package haxigniter.tests.unit;
 
-import haxigniter.libraries.Database;
-
-#if php
-import php.db.Connection;
-import php.db.ResultSet;
-#elseif neko
-import neko.db.Connection;
-import neko.db.ResultSet;
-#end
-
-class Mockconnection implements Connection
-{
-	public function new() {}
-	
-	public function close() : Void {}
-	public function commit() : Void {}
-	public function dbName() : String { return 'mockdb'; }
-	public function escape( s : String ) : String { return 'E*' + s + '*E'; }
-	public function lastInsertId() : Int { return 0; }
-	public function quote( s : String ) : String { return 'Q*' + s + '*Q'; }
-	public function request( sql : String ) : ResultSet { return new MockResultSet(); }
-	public function rollback() : Void {}
-	public function startTransaction() : Void {}
-}
-
-class Mockdatabase extends DatabaseConnection
-{
-	public function new() { }
-	
-	public override function open()
-	{
-		if(this.connection != null)
-			throw new DatabaseException('Connection is already open.', this);
-
-		this.connection = new Mockconnection();
-	}
-}
-
-class MockResultSet implements ResultSet
-{
-	public function new() { }
-	
-	private function getLength() : Int { return 0; }
-	private function getNFields() : Int { return 0; }
-	
-	public var length(getLength, null) : Int;
-	public var nfields(getNFields, null) : Int;
-	public function getFloatResult(n : Int) : Float { return 0; }
-	public function getIntResult(n : Int) : Int { return 0; }
-	public function getResult(n : Int) : String { return ''; }
-	public function hasNext() : Bool { return false; }
-	public function next() : Dynamic { return null; }
-	public function results() : List<Dynamic> { return new List(); }
-}
+import haxigniter.tests.unit.MockDatabaseConnection;
 
 class When_using_library_Database extends haxigniter.tests.TestCase
 {
-	private var db : Mockdatabase;
+	private var db : MockDatabaseConnection;
 	
 	public override function setup()
 	{
-		db = new Mockdatabase();
+		db = new MockDatabaseConnection();
 	}
 	
 	public function test_Then_dynamic_objects_should_be_iterated()
@@ -71,6 +18,9 @@ class When_using_library_Database extends haxigniter.tests.TestCase
 		var data2 = new Hash<Dynamic>();
 		data2.set('who', 'me');
 		data2.set('you', 1337);
+		
+		// Simple test of the mock object
+		this.assertEqual(0, db.queries.length);
 
 		db.insert('test', data1);
 		this.assertEqual('INSERT INTO test (me, data) VALUES (Q*who*Q, Q*you*Q)', db.lastQuery);
@@ -83,5 +33,10 @@ class When_using_library_Database extends haxigniter.tests.TestCase
 
 		db.delete('test', data1, 1);
 		this.assertEqual('DELETE FROM test WHERE me=Q*who*Q AND data=Q*you*Q LIMIT 1', db.lastQuery);	
+		
+		// Simple test of the mock object
+		this.assertEqual(4, db.queries.length);
+		this.assertEqual('INSERT INTO test (me, data) VALUES (Q*who*Q, Q*you*Q)', db.queries[0]);
+		this.assertEqual('DELETE FROM test WHERE me=Q*who*Q AND data=Q*you*Q LIMIT 1', db.queries[3]);
 	}
 }
