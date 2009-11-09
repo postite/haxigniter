7e0a07227dcd35f0a24b5eb28156fad523f7d108
Andreas Soderlund <ciscoheat@gmail.com>
2009-11-09 23:45:32 +0100

    Added haxigniter.libraries.IterableTools, for searching arrays and testing subsets of iterables.

---
+++ src/haxigniter/libraries/IterableTools.hx
@@ -0,0 +1,26 @@
+﻿package haxigniter.libraries;
+
+class IterableTools
+{
+	public static function isSubsetOf<T>(subSet : Iterable<T>, bigSet : Iterable<T>) : Bool
+	{
+		for(v in subSet)
+		{
+			if(!Lambda.has(bigSet, v))
+				return false;
+		}
+		
+		return true;
+	}
+
+	public static function arraySearch<T>(array : Array<T>, searchFor : T) : Null<Int>
+	{
+		for(i in 0 ... array.length)
+		{
+			if(array[i] == searchFor)
+				return i;
+		}
+		
+		return null;
+	}	
+}
